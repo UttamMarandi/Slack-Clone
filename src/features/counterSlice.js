da6b486d8b7bc37ced3af6e1def4b1591749065e
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-26 01:37:50 +0530

    understanding styled components

---
+++ src/features/counterSlice.js
@@ -0,0 +1,39 @@
+import {createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+
+  },
+  
+});
+
+// export const { } = counterSlice.actions;
+
+
+// export const selectCount = (state) => state.counter.value;
+
+// We can also write thunks by hand, which may contain both sync and async logic.
+// Here's an example of conditionally dispatching actions based on current state.
+
+
+export default counterSlice.reducer;
