61ec8b703bcbd6b15478f61aeebb81ee6062c478
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-26 00:22:10 +0530

    initial commit - setting firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCAiE4KwJfGVLa4pI4N0fo_jGsrwWIE958",
+  authDomain: "slack-clone-uttam.firebaseapp.com",
+  projectId: "slack-clone-uttam",
+  storageBucket: "slack-clone-uttam.appspot.com",
+  messagingSenderId: "158955099952",
+  appId: "1:158955099952:web:d8b0c9bba7c7e9773edaf5"
+};
